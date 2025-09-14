3668b043e7ec99e63ec90bcb002bb6df4de6a526
Jeff Crosley <jeffcrosley@gmail.com>
2025-09-14 17:17:22 -0400

    fix(31-module-to-standalone): basic standalone architectural conversion

---
+++ apps/angular/31-module-to-standalone/src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig } from '@angular/core';
+
+export const appConfig: ApplicationConfig = {
+  providers: [],
+};
+
+// import { MainShellModule } from '@angular-challenges/module-to-standalone/shell';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   imports: [BrowserModule, MainShellModule],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
