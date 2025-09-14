4d8a76a7feba7c12a6b3606debbd6e02c1698531
Jeff Crosley <jeffcrosley@gmail.com>
2025-09-14 13:06:30 -0400

    fix(card abstraction): city card implementation

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,9 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
-  imports: [],
+  template: `
+    <app-card
+      [list]="cities()"
+      [backgroundColor]="backgroundColor"
+      (onAddNewItem)="addNewItem()"
+      (onDeleteItem)="deleteItem($event)">
+      <img ngSrc="assets/img/city.png" width="200" height="200" />
+    </app-card>
+  `,
+  imports: [CardComponent, NgOptimizedImage],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CityCardComponent {}
+export class CityCardComponent {
+  private http = inject(FakeHttpService);
+  private store = inject(CityStore);
+
+  cities = this.store.cities;
+  backgroundColor = 'rgba(0, 0, 250, 0.1)';
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  addNewItem = () => this.store.addOne(randomCity());
+  deleteItem = (id: number) => this.store.deleteOne(id);
+}
