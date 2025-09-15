ec9e25d8bde79530b31f4948432773de62717224
Jeff Crosley <jeffcrosley@gmail.com>
2025-09-15 19:48:48 -0400

    fix(): user page routes

--- libs/module-to-standalone/shell/src/lib/main-shell.routes.ts
+++ libs/module-to-standalone/shell/src/lib/main-shell.routes.ts
@@ -30,14 +30,50 @@ export const appRoutes: Route[] = [
       },
     ],
   },
-  // {
-  //   path: 'user',
-  //   loadChildren: () =>
-  //     import('@angular-challenges/module-to-standalone/user/shell').then(
-  //       (m) => m.UserShellModule,
-  //     ),
-  // },
-
+  {
+    path: 'user',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('@angular-challenges/module-to-standalone/user/shell').then(
+            (c) => c.UserShellComponent,
+          ),
+        children: [
+          { path: '', redirectTo: 'home', pathMatch: 'full' },
+          {
+            path: 'home',
+            loadComponent() {
+              return import(
+                '@angular-challenges/module-to-standalone/user/home'
+              ).then((c) => c.UserHomeComponent);
+            },
+          },
+          {
+            path: 'contact',
+            children: [
+              {
+                path: '',
+                loadComponent() {
+                  return import(
+                    '@angular-challenges/module-to-standalone/user/contact'
+                  ).then((c) => c.ContactDashboardComponent);
+                },
+              },
+              {
+                path: 'create-contact',
+                loadComponent() {
+                  return import(
+                    '@angular-challenges/module-to-standalone/user/contact'
+                  ).then((c) => c.CreateContactComponent);
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
   {
     path: 'forbidden',
     loadComponent: () =>
